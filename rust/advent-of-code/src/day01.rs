42c80e94f55b5dd9b8082e99f2b4074d48ff8887
Devon Finninger <dfinninger@tesla.com>
2018-12-01 10:34:25 -0800

    add rust

---
+++ rust/advent-of-code/src/day01.rs
@@ -0,0 +1,29 @@
+// Day 01
+
+use std::collections::HashSet;
+
+pub fn part1() {
+    let input = include_str!("day01.txt");
+    let x: i32 = input.lines()
+        .map(|i: &str| i.parse::<i32>().unwrap())
+        .sum();
+
+    println!("part 1: {}", x);
+}
+
+pub fn part2() {
+    let input = include_str!("day01.txt");
+    let endless = input.lines()
+        .map(|i: &str| i.parse::<i32>().unwrap())
+        .cycle();
+
+    let mut state = 0;
+    let mut states = HashSet::new();
+    for item in endless {
+        state += item;
+        if ! states.insert(state) {
+            println!("part 2: {}", state);
+            return
+        }
+    }
+}
---
+++ rust/advent-of-code/src/main.rs
@@ -0,0 +1,7 @@
+mod day01;
+
+fn main() {
+    println!("--- day 01 ---");
+    day01::part1();
+    day01::part2();
+}
